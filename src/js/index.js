fde744f39b006a7ecd9007ecfa2f6ae8f29a67af
digua <453191147@qq.com>
2020-03-23 15:24:58 +0800

    初始化提交

---
+++ src/js/index.js
@@ -0,0 +1,7 @@
+import "@babel/polyfill"
+import "business/about.js"
+import "business/course.js"
+import "business/home.js"
+import "business/team.js"
+import "business/works.js"
+import "frame/frame.js"
