668f85a388b28dc0201f291b3d309f5a188d4715
digua <453191147@qq.com>
2020-03-28 15:16:19 +0800

    frame第一次提交

---
+++ src/js/frame/move/move.js
@@ -0,0 +1,21 @@
+
+export default function(index){
+  var liNodes=document.querySelectorAll( "html body #app .head .headerMain .list .item");
+  var arrowNode=document.querySelector("html body #app .head .headerMain .arrow")
+  var contentNode=document.querySelector("html body #app .content ");
+  var listNode=document.querySelector("html body #app .content .list");
+  var pointsLiNodes=document.querySelectorAll("html body #app .content .points li");
+  contentNode.index=index
+
+  for (var j=0;j<liNodes.length;j++){
+    liNodes[j].classList.remove("active")
+    pointsLiNodes[j].classList.remove("active")
+  }
+  liNodes[index].classList.add("active");
+  pointsLiNodes[index].classList.add("active");
+
+  arrowNode.style.left=(liNodes[index].offsetLeft+(liNodes[index].offsetWidth/2)-(arrowNode.offsetWidth/2))+"px"
+
+  listNode.style.top=`${-(index*contentNode.offsetHeight)}px`
+}
+
--- src/js/index.js
+++ src/js/index.js
@@ -6,5 +6,14 @@ import "./business/team.js"
 import "./business/works.js"
 import "./frame/frame.js"
 
-
+window.onresize= ()=> {
+  var contentLiNodes=document.querySelectorAll("html body #app .content .list li");
+  var contentNode=document.querySelector("html body #app .content ");
+  var arrowNode=document.querySelector("html body #app .head .headerMain .arrow")
+  var liNodes=document.querySelectorAll( "html body #app .head .headerMain .list .item");
+  contentLiNodes.forEach((li)=>{
+    li.style.height=(contentNode.offsetHeight)+"px";
+  })
+  arrowNode.style.left=(liNodes[0].offsetLeft+(liNodes[0].offsetWidth/2)-(arrowNode.offsetWidth/2))+"px"
+}
 
